af083d64a164fb2a7deb241435a7cfc43a77f492
ruben <ruben.lamon@gmail.com>
2025-09-22 21:26:32 +0200

    add comment

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -5,6 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
+    // testje
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
